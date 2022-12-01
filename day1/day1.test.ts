50c23d4d70c975fd4df17ce9121a885d3de6f011
Neil Gall <neil@gall.scot>
2022-12-01 07:32:57 +0000

    Day 1 part 2

--- day1/day1.test.ts
+++ day1/day1.test.ts
@@ -1,4 +1,4 @@
-import { parse_input, part1 } from "./day1";
+import { parse_input, part1, part2 } from "./day1";
 
 describe("parser", () => {
     it("reads the test data", () => {
@@ -22,9 +22,14 @@ describe("parser", () => {
 });
 
 
-describe("part1", () => {
-    it("calculates the right answer", () => {
-        const elves = [6000, 4000, 11000, 24000, 10000];
+describe("problems", () => {
+    const elves = [6000, 4000, 11000, 24000, 10000];
+
+    it("part1", () => {
         expect(part1(elves)).toBe(24000);
+    });
+
+    it("part2", () => {
+        expect(part2(elves)).toBe(45000);
     });
 });
--- day1/day1.ts
+++ day1/day1.ts
@@ -21,7 +21,13 @@ export function part1(elves: number[]): number {
     return Math.max(...elves);
 }
 
+export function part2(elves: number[]): number {
+    const sorted = elves.sort((n1, n2) => n2 - n1);
+    return sorted.slice(0, 3).reduce((sum, n) => sum + n);
+}
+
 const input = readFileSync("input.txt", "utf8").toString();
 const elves = parse_input(input);
 
 console.log(`Part 1: ${part1(elves)}`);
+console.log(`Part 2: ${part2(elves)}`);
