ffed5b4234d31085cc70e4007d94b7cf25ba9c7f
Neil Gall <neil@gall.scot>
2022-12-02 09:36:10 +0000

    Day 2 part 2

--- day2/day2.test.ts
+++ day2/day2.test.ts
@@ -1,37 +1,71 @@
-import { Move, parseInput, score, part1, part2 } from "./day2";
+import { 
+    Move,
+    Result,
+    parseInput1,
+    parseInput2,
+    score1,
+    score2,
+    part1,
+    part2 
+} from "./day2";
 
-const exampleStrategy = [
+const exampleStrategy1 = [
     { opponent: Move.Rock, you: Move.Paper },
     { opponent: Move.Paper, you: Move.Rock },
     { opponent: Move.Scissors, you: Move.Scissors }
 ];
 
+const exampleStrategy2 = [
+    { opponent: Move.Rock, result: Result.Draw },
+    { opponent: Move.Paper, result: Result.Lose },
+    { opponent: Move.Scissors, result: Result.Win }
+];
+
+
 describe("parser", () => {
-    it("reads the test data", () => {
-        const strategy = parseInput(
+    const testInput = 
 `A Y
 B X
-C Z`);
-        expect(strategy).toStrictEqual(exampleStrategy);
+C Z`;
+
+    it("reads the test data for part 1", () => {
+        const strategy = parseInput1(testInput);
+        expect(strategy).toStrictEqual(exampleStrategy1);
+    });
+
+    it("reads the test data for part 2", () => {
+        const strategy = parseInput2(testInput);
+        expect(strategy).toStrictEqual(exampleStrategy2);
     });
 });
 
-describe("score", () => {
+describe("score1", () => {
     it.each([
-        [exampleStrategy[0], 8],
-        [exampleStrategy[1], 1],
-        [exampleStrategy[2], 6]
+        [exampleStrategy1[0], 8],
+        [exampleStrategy1[1], 1],
+        [exampleStrategy1[2], 6]
 ])("%s", (round, expectedScore) => {
-        expect(score(round)).toBe(expectedScore);
+        expect(score1(round)).toBe(expectedScore);
+    });
+});
+
+
+describe("score2", () => {
+    it.each([
+        [exampleStrategy2[0], 4],
+        [exampleStrategy2[1], 1],
+        [exampleStrategy2[2], 7]
+])("%s", (round, expectedScore) => {
+        expect(score2(round)).toBe(expectedScore);
     })
 })
 
-
 describe("problems", () => {
     it("part1", () => {
-        expect(part1(exampleStrategy)).toBe(15);
+        expect(part1(exampleStrategy1)).toBe(15);
     });
 
     it("part2", () => {
+        expect(part2(exampleStrategy2)).toBe(12);
     });
 });
--- day2/day2.ts
+++ day2/day2.ts
@@ -12,18 +12,24 @@ export enum Result {
     Win = 6
 };
 
-export type Round = {
+export type Round1 = {
     opponent: Move;
     you: Move;
 };
 
-type StrategyGuide = Round[];
+export type Round2 = {
+    opponent: Move;
+    result: Result;
+};
+
+type StrategyGuide1 = Round1[];
+type StrategyGuide2 = Round2[];
 
 function sum(ns: number[]): number {
     return ns.reduce((s, n) => s + n);
 }
 
-function playRound(round: Round): Result {
+function playRound1(round: Round1): Result {
     if (round.you === round.opponent) {
         return Result.Draw;
     }
@@ -43,11 +49,35 @@ function playRound(round: Round): Result {
     }
 }
 
-export function score(round: Round): number {
-    return round.you + playRound(round);
+function playRound2(round: Round2): Move {
+    if (round.result === Result.Draw) {
+        return round.opponent;
+    }
+    switch (round.opponent) {
+        case Move.Rock:
+            return (round.result === Result.Win) 
+                ? Move.Paper
+                : Move.Scissors;
+        case Move.Paper:
+            return (round.result === Result.Win)
+                ? Move.Scissors
+                : Move.Rock;
+        case Move.Scissors:
+            return (round.result === Result.Win)
+                ? Move.Rock
+                : Move.Paper;
+    }
 }
 
-export function parseInput(input: string): StrategyGuide {
+export function score1(round: Round1): number {
+    return round.you + playRound1(round);
+}
+
+export function score2(round: Round2): number {
+    return round.result + playRound2(round);
+}
+
+export function parseInput1(input: string): StrategyGuide1 {
     const decode: Record<string, Move> = {
         "A": Move.Rock,
         "B": Move.Paper,
@@ -65,16 +95,39 @@ export function parseInput(input: string): StrategyGuide {
     });
 }
 
-export function part1(strategy: StrategyGuide): number {
-    return sum(strategy.map(score));
+export function parseInput2(input: string): StrategyGuide2 {
+    const decodeMove: Record<string, Move> = {
+        "A": Move.Rock,
+        "B": Move.Paper,
+        "C": Move.Scissors,
+    };
+    const decodeResult: Record<string, Result> = {
+        "X": Result.Lose,
+        "Y": Result.Draw,
+        "Z": Result.Win,
+    };
+
+    const lines = input.trim().split("\n");
+    return lines.map((line) => {
+        const [opponent, result] = line.split(" ");
+        return { 
+            opponent: decodeMove[opponent],
+            result: decodeResult[result]
+        };
+    });
 }
 
-export function part2(strategy: StrategyGuide): number {
-    return 0;
+export function part1(strategy: StrategyGuide1): number {
+    return sum(strategy.map(score1));
+}
+
+export function part2(strategy: StrategyGuide2): number {
+    return sum(strategy.map(score2));
 }
 
 const input = readFileSync("input.txt", "utf8").toString();
-const strategy = parseInput(input);
+const strategy1 = parseInput1(input);
+const strategy2 = parseInput2(input);
 
-console.log(`Part 1: ${part1(strategy)}`);
-console.log(`Part 2: ${part2(strategy)}`);
+console.log(`Part 1: ${part1(strategy1)}`);
+console.log(`Part 2: ${part2(strategy2)}`);
