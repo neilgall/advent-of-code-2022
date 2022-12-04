544b989cb7867e9857312102fcb6b897766b3d46
Neil Gall <neil@gall.scot>
2022-12-04 13:41:40 +0000

    Add new day script

---
+++ new-day.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eu
+
+fail() {
+    echo "ERROR: $*"
+    exit 1
+}
+
+day=${1:-}
+if [ -z "${day}" ]; then
+    day=$(printf "%(%e)T" | xargs)
+fi
+
+dir="day${day}"
+test ! -d "$dir" || fail "${dir} already exists"
+
+cp -R template "day${day}"
+sed -i "s/dayX/${dir}/" "${dir}/package.json"
