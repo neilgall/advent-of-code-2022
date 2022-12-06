b9424fc789b31b04bb5da410a375c0692a451a09
Neil Gall <neil@gall.scot>
2022-12-06 07:52:16 +0000

    Day 6 part 2

--- day6/solution.test.ts
+++ day6/solution.test.ts
@@ -1,4 +1,4 @@
-import { part1 } from "./solution";
+import { part1, part2 } from "./solution";
 
 
 describe("part1", () => {
@@ -10,10 +10,18 @@ describe("part1", () => {
         ["zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11]
     ])("calculates the position for %s", (input, expected) => {
         expect(part1(input)).toBe(expected);
-    })
+    });
 });
 
 
 describe("part2", () => {
-
+    it.each([
+        ["mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19],
+        ["bvwbjplbgvbhsrlpgdmjqwftvncz", 23],
+        ["nppdvjthqldpwncqszvftbrmjlhg", 23],
+        ["nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29],
+        ["zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26],
+    ])("calculates the position for %s", (input, expected) => {
+        expect(part2(input)).toBe(expected);
+    });
 });
--- day6/solution.ts
+++ day6/solution.ts
@@ -2,15 +2,19 @@ export function hasDuplicateChars(s: string): boolean {
     return new Set(s.split("")).size < s.length;
 }
 
-export function part1(input: string): number | undefined {
-    for (let i = 4; i < input.length; ++i) {
-        const s = input.substring(i - 4, i);
-        if (!hasDuplicateChars(s)) {
+export function findUniqueSubstring(s: string, len: number): number | undefined {
+    for (let i = len; i < s.length; ++i) {
+        const ss = s.substring(i - len, i);
+        if (!hasDuplicateChars(ss)) {
             return i;
         }
     }
 }
 
-export function part2(input: string): number {
-    return 0;
+export function part1(input: string): number | undefined {
+    return findUniqueSubstring(input, 4);
+}
+
+export function part2(input: string): number | undefined {
+    return findUniqueSubstring(input, 14);
 }
