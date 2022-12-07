3216ecb0a6060539d715e30cd83e2c915391e1bf
Neil Gall <neil@gall.scot>
2022-12-07 23:30:43 +0000

    Improve test template

--- template/solution.test.ts
+++ template/solution.test.ts
@@ -1,3 +1,11 @@
+import { 
+    part1,
+    part2,
+ } from "./solution";
+
+const testInput = 
+``;
+
 describe("parseInput", () => {
     it("parses the input", () => {
 
@@ -6,10 +14,14 @@ describe("parseInput", () => {
 
 
 describe("part1", () => {
-
+    it("calculates the correct answer", () => {
+        expect(part1(testInput)).toBe(0);
+    });
 });
 
 
 describe("part2", () => {
-
+    it("calculates the correct answer", () => {
+        expect(part2(testInput)).toBe(0);
+    });
 });
