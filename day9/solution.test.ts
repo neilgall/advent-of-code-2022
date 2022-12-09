986c1d2da84f4c91be3740136e4400294500f137
Neil Gall <neil@gall.scot>
2022-12-09 09:17:52 +0000

    Day 9 part 1 (passing tests but no correct answer)

--- day9/solution.test.ts
+++ day9/solution.test.ts
@@ -1,8 +1,12 @@
 import { 
     Direction,
+    Point,
+    point,
     parseInput,
     part1,
     part2,
+    pointKey,
+    moveTail,
  } from "./solution";
 
 const testInput = 
@@ -30,10 +34,57 @@ describe("parseInput", () => {
     });
 });
 
+describe("Point", () => {
+    it("can be inserted into a record", () => {
+        const ps: Record<string, string> = {};
+        ps[pointKey({ x: 3, y: 4 })] = "foo";
+        ps[pointKey({ x: 4, y: 3 })] = "bar";
+
+        expect(ps[pointKey({ x: 3, y: 4 })]).toBe("foo");
+        expect(ps[pointKey({ x: 4, y: 3 })]).toBe("bar");
+    })
+});
+
+describe("moveTail", () => {
+    it.each([
+        // 2 rows above
+        [point(0, 0), point(1, 1)],
+        [point(1, 0), point(2, 1)],
+        [point(2, 0), point(2, 1)],
+        [point(3, 0), point(2, 1)],
+        [point(4, 0), point(3, 1)],
+        // 1 row above
+        [point(0, 1), point(1, 2)],
+        [point(1, 1), point(1, 1)],
+        [point(2, 1), point(2, 1)],
+        [point(3, 1), point(3, 1)],
+        [point(4, 1), point(3, 2)],
+        // same row
+        [point(0, 2), point(1, 2)],
+        [point(1, 2), point(1, 2)],
+        [point(2, 2), point(2, 2)],
+        [point(3, 2), point(3, 2)],
+        [point(4, 2), point(3, 2)],
+        // 1 row below
+        [point(0, 3), point(1, 2)],
+        [point(1, 3), point(1, 3)],
+        [point(2, 3), point(2, 3)],
+        [point(3, 3), point(3, 3)],
+        [point(4, 3), point(3, 2)],
+        // 2 rows below
+        [point(0, 4), point(1, 3)],
+        [point(1, 4), point(2, 3)],
+        [point(2, 4), point(2, 3)],
+        [point(3, 4), point(2, 3)],
+        [point(4, 4), point(3, 3)],
+    ])("moves tail correctly %s -> %s", (tail, expected) => {
+        expect(moveTail(point(2, 2), tail)).toStrictEqual(expected);
+    });
+})
 
 describe("part1", () => {
     it("calculates the correct answer", () => {
-        expect(part1(testInput)).toBe(0);
+        expect(part1(testInput)).toBe(13);
     });
 });
 
--- day9/solution.ts
+++ day9/solution.ts
@@ -19,13 +19,121 @@ export function parseInput(input: string): Move[] {
         const direction = dirMap[d];
         const steps = Number(s);
         if (direction === undefined)
-        throw new Error(`can't parse '${d}'`);
+            throw new Error(`can't parse '${d}'`);
         return { direction, steps };
     });
 }
 
+export type Point = {
+    x: number;
+    y: number;
+};
+
+export function point(x: number, y: number): Point {
+    return { x, y };
+}
+
+export function pointKey(p: Point): string {
+    return `${p.x},${p.y}`;
+}
+
+export function keyPoint(s: string): Point {
+    const [x, y] = s.split(",").map(Number);
+    return { x, y };
+}
+
+export function movePoint(p: Point, dir: Direction): Point {
+    switch (dir) {
+        case Direction.Up:
+            return { x: p.x, y: p.y-1 };
+        case Direction.Down:
+            return { x: p.x, y: p.y+1 };
+        case Direction.Left:
+            return { x: p.x-1, y: p.y };
+        case Direction.Right:
+            return { x: p.x+1, y: p.y };
+    };
+}
+
+export type Grid = {
+    head: Point;
+    tail: Point;
+    visited: Set<string>;
+};
+
+export function moveTail(head: Point, tail: Point): Point {
+    const dx = Math.abs(head.x - tail.x);
+    const dy = Math.abs(head.y - tail.y);
+    if (dx < 2 && dy < 2) {
+        return tail;
+    }
+
+    if (dx === 0) {
+        return head.y > tail.y
+            ? movePoint(tail, Direction.Down)
+            : movePoint(tail, Direction.Up);
+    } 
+    if (dy === 0) {
+        return head.x > tail.x
+            ? movePoint(tail, Direction.Right)
+            : movePoint(tail, Direction.Left);
+    }
+
+    let newTail = head.y > tail.y
+        ? movePoint(tail, Direction.Down)
+        : movePoint(tail, Direction.Up);
+    return head.x > tail.x
+        ? movePoint(newTail, Direction.Right)
+        : movePoint(newTail, Direction.Left);
+}
+
+export function applyMove(g: Grid, d: Direction): Grid {
+    const head = movePoint(g.head, d);
+    const tail = moveTail(g.head, g.tail);
+    const visited = g.visited.add(pointKey(tail));
+    return { head, tail, visited };
+}
+
+export function applyMoves(g: Grid, ms: Move[]): Grid {
+    let grid = { ...g };
+    for (const m of ms) {
+        for (let i = 0; i < m.steps; ++i) {
+            grid = applyMove(grid, m.direction);
+        }
+    }
+    return grid;
+}
+
+const initialGrid = {
+    head: point(0, 0),
+    tail: point(0, 0),
+    visited: new Set([pointKey(point(0, 0))]),
+};
+
+export function render(g: Grid) {
+    let minx = 0, maxx = 0, miny = 0, maxy = 0;
+    g.visited.forEach((k) => {
+        const p = keyPoint(k);
+        minx = Math.min(minx, p.x);
+        maxx = Math.max(maxx, p.x);
+        miny = Math.min(miny, p.y);
+        maxy = Math.max(maxy, p.y);
+    });
+    let box = "";
+    for (let y = miny; y <= maxy; ++y) {
+        let row = "";
+        for (let x = minx; x <= maxx; ++x) {
+            row += g.visited.has(pointKey(point(x, y))) ? "#" : ".";
+        }
+        box += row + "\n";
+    }
+    console.log(box);
+}
+
 export function part1(input: string): number {
-    return 0;
+    const moves = parseInput(input);
+    const grid = applyMoves(initialGrid, moves);
+    return grid.visited.size;
 }
 
 export function part2(input: string): number {
