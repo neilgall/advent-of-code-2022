4f5243b294fb5e0f694e4e680aea19bbd153b51e
Neil Gall <neil@gall.scot>
2022-12-13 20:25:07 +0000

    Day 13 part 1

--- day13/solution.test.ts
+++ day13/solution.test.ts
@@ -1,4 +1,6 @@
 import { 
+    comparePacketPair,
+    CompareResult,
     parseInput,
     part1,
     part2,
@@ -50,10 +52,24 @@ describe("parseInput", () => {
     });
 });
 
+describe("comparePacketPair", () => {
+    it.each([
+        [[1,1,3,1,1], [1,1,5,1,1], CompareResult.Ordered],
+        [[[1],[2,3,4]], [[1],4], CompareResult.Ordered],
+        [[9], [[8,7,6]], CompareResult.NotOrdered],
+        [[[4,4],4,4], [[4,4],4,4,4], CompareResult.Ordered],
+        [[7,7,7,7], [7,7,7], CompareResult.NotOrdered],
+        [[], [3], CompareResult.Ordered],
+        [[[[]]], [[]], CompareResult.NotOrdered],
+        [[1,[2,[3,[4,[5,6,7]]]],8,9], [1,[2,[3,[4,[5,6,0]]]],8,9], CompareResult.NotOrdered]
+    ])("%s vs %s should be %s", (left, right, expected) => {
+        expect(comparePacketPair([left, right])).toBe(expected);
+    });
+});
 
 describe("part1", () => {
     it("calculates the correct answer", () => {
-        expect(part1(testInput)).toBe(0);
+        expect(part1(testInput)).toBe(13);
     });
 });
 
--- day13/solution.ts
+++ day13/solution.ts
@@ -7,6 +7,7 @@ import {
     Parser,
     ParserRef
 } from "../lib/parser";
+import { sum } from "./utils";
 
 export type PacketData = number | PacketData[];
 
@@ -45,9 +46,52 @@ export function parseInput(input: string): PacketPair[] {
     });
 }
 
+export enum CompareResult {
+    Ordered,
+    NotOrdered,
+    Unknown,
+};
+
+export function isNumber(x: any): boolean {
+    return typeof(x) === "number";
+}
+
+export function comparePacketPair(pp: PacketPair): CompareResult {
+    const [left, right] = pp;
+    if (isNumber(left) && isNumber(right)) {
+        if (left < right) return CompareResult.Ordered;
+        else if (left > right) return CompareResult.NotOrdered;
+        else return CompareResult.Unknown;
+    }
+    else if (isNumber(left)) {
+        return comparePacketPair([[left], right]);
+    } else if (isNumber(right)) {
+        return comparePacketPair([left, [right]]);
+    } else {
+        const l = left as PacketData[];
+        const r = right as PacketData[];
+        for (let i = 0; i < l.length || i < r.length; ++i) {
+            if (i == r.length) {
+                return CompareResult.NotOrdered;
+            } else if (i == l.length) {
+                return CompareResult.Ordered;
+            } else {
+                const c = comparePacketPair([l[i], r[i]]);
+                if (c !== CompareResult.Unknown) {
+                    return c;
+                }
+            }
+        }
+        return CompareResult.Unknown;
+    }
+}
+
 
 export function part1(input: string): number {
-    return 0;
+    const pairs = parseInput(input);
+    return sum(pairs.map((p, i) =>
+        comparePacketPair(p) === CompareResult.Ordered ? i+1 : 0
+    ));
 }
 
 export function part2(input: string): number {
