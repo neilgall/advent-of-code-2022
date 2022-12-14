400769cd61b4450c0408cbea80146ad028058964
Neil Gall <neil@gall.scot>
2022-12-14 13:20:06 +0000

    Day 14: parsing

--- day14/solution.test.ts
+++ day14/solution.test.ts
@@ -1,14 +1,20 @@
 import { 
+    parseInput,
     part1,
     part2,
  } from "./solution";
 
 const testInput = 
-``;
+`498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9
+`;
 
 describe("parseInput", () => {
     it("parses the input", () => {
-
+        expect(parseInput(testInput)).toStrictEqual([
+            [{ x: 498, y: 4 }, { x: 498, y: 6 }, { x: 496, y: 6 }],
+            [{ x: 503, y: 4 }, { x: 502, y: 4 }, { x: 502, y: 9 }, { x: 494, y: 9 }]
+        ]);
     });
 });
 
--- day14/solution.ts
+++ day14/solution.ts
@@ -1,4 +1,24 @@
+import { triple, number, literal, Parser, whitespace } from "../lib/parser";
 import { Point, PointMap } from "../lib/pointMap";
+
+export type Trace = Point[];
+
+export function parseInput(input: string): Trace[] {
+    const point: Parser<Point> = triple(
+        number,
+        literal(","),
+        number,
+        (x, _, y) => ({ x, y })
+    );
+
+    const trace: Parser<Trace> = point.sepBy(literal(" -> "));
+
+    const traces = input.trim().split("\n").map(trace.parse);
+    return traces.map((t, i) => {
+        if (t.ok) return t.value;
+        else throw new Error(`line ${i}: expected: ${t.expected} at ${t.at}`);
+    });
+}
 
 export function part1(input: string): number {
     return 0;
